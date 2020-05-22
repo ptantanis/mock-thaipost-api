7a59f2986e670c4b85fc95fc38483b4bd21c4185
Pachara Tantanis <pachara.tantanis@thoughtworks.com>
2020-05-23 02:42:53 +0700

    add log current port

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import express from 'express'
 
 // Create a new express app instance
 const app: express.Application = express();
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.send('Hello World2!');
 });
-app.listen(process.env.PORT || 3000, function () {
-    console.log('App is listening on port 3000!');
+app.listen(port, function () {
+    console.log('App is listening on port ' + port);
 });
